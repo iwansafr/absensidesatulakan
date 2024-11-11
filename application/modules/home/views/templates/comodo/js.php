2051578bb3a5670decb342ef9f00bef470659fa8
iwansafr <iwansafr@gmail.com>
2024-11-11 08:40:07 +0700

    remove google id

--- application/modules/home/views/templates/comodo/js.php
+++ application/modules/home/views/templates/comodo/js.php
@@ -11,7 +11,5 @@
 <script src="<?php echo $link_template ?>/vendors/counter-up/jquery.waypoints.min.js"></script>
 <script src="<?php echo $link_template ?>/vendors/counter-up/jquery.counterup.min.js"></script>
 <!-- <script src="<?php echo $link_template ?>/js/mail-script.js"></script> -->
-<!--gmaps Js-->
-<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script> -->
 <script src="<?php echo $link_template ?>/js/gmaps.min.js"></script>
 <script src="<?php echo $link_template ?>/js/theme.js"></script>
